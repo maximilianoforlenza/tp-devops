38c7a5102091952bba5047e127b281635fce427d
maximiliano <mforlenza@maximilianos-MacBook-Pro.local>
2026-05-19 15:47:29 -0300

    chore(publish): publish docer imagen when a new tag is created on master

--- instrumentation.mjs
+++ instrumentation.mjs
@@ -3,12 +3,7 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
-import { Resource } from '@opentelemetry/resources'
-
 const sdk = new NodeSDK({
-  resource: new Resource({
-    'service.name': process.env.OTEL_SERVICE_NAME || 'tp-devops'
-  }),
   traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter()
